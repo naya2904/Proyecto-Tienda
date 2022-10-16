719ebd6749ca86830bb09527734583cfdd0bcc64
naya2904 <nayarit2904@gmail.com>
2022-10-16 15:15:42 -0600

    Semana 5

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -2,27 +2,52 @@ package com.tienda.controller;
 
 import com.tienda.Dao.ClienteDao;
 import com.tienda.domain.Cliente;
+import com.tienda.service.ClienteService;
 import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
 public class IndexController {
     @Autowired
-    private ClienteDao clienteDao;
+    private ClienteService clienteService;
     
          @GetMapping  ("/")
           public String inicio (Model model) {
              log.info("Estamos usando una arquitectura mvc");
              
-             var clientes=clienteDao.findAll();    
-             model.addAttribute ("clientes",clientes);
+             var clientes=clienteService.getClientes();    
+        model.addAttribute("clientes",clientes);
+             
              return "index";
     }
     
-
+        @GetMapping ("/nuevoCliente")
+        public String nuevoCliente (Cliente cliente) {
+            return "modficarCliente";       
+  }
+        @PostMapping ("/guardarCliente")
+        public String guardarCliente (Cliente cliente) {
+            clienteService.save(cliente);
+            return "redirect:/";
+            
+        }
+        
+        @GetMapping ("/modificarCliente/ {idCliente}")
+        public String modificarCliente (Cliente cliente, Model model) {
+            cliente = clienteService.getCliente (cliente);
+            model.addAttribute("cliente", cliente);
+            return "modificarCliente";
+        }
+        
+        @GetMapping ("/eliminarCliente/ {idCliente}")
+        public String modificarCliente (Cliente cliente) {
+            clienteService.delete (cliente);
+            return "redirect:/";
+        }
 }
--- src/main/java/com/tienda/domain/Cliente.java
+++ src/main/java/com/tienda/domain/Cliente.java
@@ -18,7 +18,7 @@ public class Cliente implements Serializable {
     
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
-    private Long idcliente;
+    private Long idCliente;
     private String nombre;
     private String apellidos;
     private String correo;
